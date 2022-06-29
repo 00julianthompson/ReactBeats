898f13ae36ced8e0200ac1b37b8f778dfae74244
unknown <unsungpoetry1993@gmail.com' git config --global user.name 00julianthompsongit config user.name  git commit -m Added>
2022-06-29 00:06:39 -0400

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import "./Navbar.css";
+
+function NavBar() {
+    return (
+        <div className="NavBar">
+      <h1 className="Page_Title">
+          TRUST
+
+      </h1>
+      <ul>
+        <li><a href="/Home">Home</a></li>
+        <li><a href="/Beats">Beats</a></li>
+        <li><a href="/Loops">Loops</a></li>
+        <li><a href="/Demos">Demos</a></li>
+      </ul>
+    </div>
+    )
+
+}
+
+
+export default NavBar;
