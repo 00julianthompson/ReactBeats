c4560f6db573b0efe82e490950bfdc820c757422
unknown <unsungpoetry1993@gmail.com' git config --global user.name 00julianthompsongit config user.name  git commit -m Added>
2022-07-13 00:02:39 -0400

    Took a different route

---
+++ src/components/Player.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PlayerDetails from './PlayerDetails';
+
+function Player(props) {
+  return (
+    <div className='c-player'>
+        <audio></audio>
+        <h4>Playing Now</h4>
+        <PlayerDetails song={props.song}/>
+        {/* {Controls} */}
+        <p><strong>Next up:</strong>{props.nextSong.title} by {props.nextSong.artist}</p>
+    </div>
+  )
+}
+
+export default Player;
